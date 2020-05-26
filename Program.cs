69c1d107888646d16d48d2c739e1b8799c79cd6c
jay71294-dev <jpatels71294@gmail.com>
2020-05-26 19:55:57 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloname
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Jay!");
+        }
+    }
+}
