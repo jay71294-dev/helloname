b71e4a3c6e995dc3ebee3f7eba00e2b18dd17570
jay71294-dev <jpatels71294@gmail.com>
2020-05-28 12:43:20 -0400

    Feature: Added favorite icecream

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace helloname
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Jay!");
+            string fname = "Jay";
+            string lname = "Patel";
+            string icecream = "Cookies N Cream";
+            Console.WriteLine("Hello my name is {0} {1}", fname, lname);
+            Console.WriteLine("My favorite icecream is {0}", icecream);
         }
     }
 }
