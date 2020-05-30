e76f5a9884eafd149f6855c52f574be76b345d7f
jay71294-dev <jpatels71294@gmail.com>
2020-05-30 12:39:28 -0400

    Feature:Added Game

--- Program.cs
+++ Program.cs
@@ -10,8 +10,10 @@ namespace helloname
             string fname = "Jay";
             string lname = "Patel";
             string icecream = "Cookies N Cream";
+            string game1 = "Xbox One X";
             Console.WriteLine("Hello my name is {0} {1}", fname, lname);
             Console.WriteLine("My favorite icecream is {0}", icecream);
+            Console.WriteLine("I love playing Fifa on {0}", game1);
         }
     }
 }
